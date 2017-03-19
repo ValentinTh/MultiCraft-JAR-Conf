17d0ae818150dd5a27663f6d5fe84cb846270cd7
Valentin T <ins.mrs025@outlook.com>
2017-03-19 21:29:57 +0100

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Custom config get
+echo "Let's start getting all this things baby !"
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/custom.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/custom-bungee.jar.conf
+echo "Well well well, that's the end you get all files ! Great !"
+echo "Goodbye"
+exit
