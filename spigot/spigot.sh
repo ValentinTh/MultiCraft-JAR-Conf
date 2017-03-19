27cae8695c347aca2c69ed2a0a54737f8a86990f
Valentin T <ins.mrs025@outlook.com>
2017-03-19 18:33:36 +0100

    Links successful modified

--- spigot/spigot.sh
+++ spigot/spigot.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Spigot config get
 echo "Let's start getting all this things baby !"
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-x-latest.jar.
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.11.2.jar.
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.10.2.jar
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.9.4.jar
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.8.8.jar.
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.7.10-b1657.jar
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.7.2-b1339.jar
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.6.4.jar
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.5.2.jar.conf
-wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JARConf/master/spigot/spigot-1.4.7.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-x-latest.jar.
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.11.2.jar.
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.10.2.jar
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.9.4.jar
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.8.8.jar.
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.7.10-b1657.jar
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.7.2-b1339.jar
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.6.4.jar
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.5.2.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.4.7.jar.conf
 echo "Well well well, that's the end you get all files ! Great !"
 echo "Goodbye"
 exit
