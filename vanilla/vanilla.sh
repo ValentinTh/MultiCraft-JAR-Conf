3751a93268336c7552fac29bedbcc2074250032c
Valentin T <ins.mrs025@outlook.com>
2017-03-19 19:55:35 +0100

    Create vanilla.sh

---
+++ vanilla/vanilla.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Vanilla config get
+echo "Let's start getting all this things baby !"
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.11.2.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.10.2.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.9.4.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.8.9.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.7.10.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.7.2.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.6.4.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.5.2.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.4.7.jar.conf
+echo "Well well well, that's the end you get all files ! Great !"
+echo "Goodbye"
+exit
