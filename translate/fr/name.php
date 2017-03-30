4e42a8faef33d60fd94396f3c4a10bd0db20e027
Valentin T <ins.mrs025@outlook.com>
2017-03-30 22:48:55 +0200

    Create name.php

---
+++ translate/fr/name.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'short'=>'',    //Short language code, for example 'de'
+    'english'=>'',  //English name of the language
+    'local'=>'',    //Local name of the language
+);
