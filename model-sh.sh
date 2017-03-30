5bb12af3b0a99b9150d07ed482494b55219a31ca
Valentin T <ins.mrs025@outlook.com>
2017-03-30 21:40:34 +0200

    New model

---
+++ model-sh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## xxxx config get
+echo -e "
+  __  __       _ _   _                 __ _                 _   _    ____     ____             __ _       
+ |  \/  |_   _| | |_(_) ___ _ __ __ _ / _| |_              | | / \  |  _ \   / ___|___  _ __  / _(_) __ _ 
+ | |\/| | | | | | __| |/ __|  __/ _  | |_| __|  _____   _  | |/ _ \ | |_) | | |   / _ \|  _ \| |_| |/ _  |
+ | |  | | |_| | | |_| | (__| | | (_| |  _| |_  |_____| | |_| / ___ \|  _ <  | |__| (_) | | | |  _| | (_| |
+ |_|  |_|\__,_|_|\__|_|\___|_|  \__,_|_|  \__|          \___/_/   \_\_| \_\  \____\___/|_| |_|_| |_|\__, |
+                                                                                                    |___/ "
+echo -e "\033[0;36m ## \033[0;32mLet's start getting all this things baby ! \033[37m"
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/xxxx.jar.conf
+wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/xxxx.jar.conf
+echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33mXXXXXXX \033[0;32mfiles ! Great ! \033[37m"
+echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
+exit
