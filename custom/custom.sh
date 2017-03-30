4821456f5f3eadbd77b6cc0c05186d426f3542fa
Valentin T <ins.mrs025@outlook.com>
2017-03-30 19:14:41 +0200

    Colors

--- custom/custom.sh
+++ custom/custom.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-# Custom config get
-echo "Let's start getting all this things baby !"
+## Custom config get
+# Colors
+Reset='\033[0m'       # Text Reset
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+########
+
+echo "\033[0;36mLet's start getting all this things baby !"
 wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/custom/custom.jar.conf
 wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/custom/custom-bungee.jar.conf
-echo "Well well well, that's the end you get all files ! Great !"
-echo "Goodbye"
+echo "\033[0;37mWell well well, that's the end you get all files ! Great !"
+echo "\033[0;31mGoodbye"
 exit
