abd45addff8f446a024dd898463049bb288f76aa
Valentin T <ins.mrs025@outlook.com>
2017-03-30 21:39:10 +0200

    Final edit

--- custom/custom.sh
+++ custom/custom.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 ## Custom config get
-# Colors
-Reset='\033[0m'       # Text Reset
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # Whit
-########
 echo -e "
   __  __       _ _   _                 __ _                 _   _    ____     ____             __ _       
  |  \/  |_   _| | |_(_) ___ _ __ __ _ / _| |_              | | / \  |  _ \   / ___|___  _ __  / _(_) __ _ 
