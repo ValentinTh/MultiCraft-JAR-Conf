3106363890164042581cdd6903843817a79fb6ef
Valentin T <ins.mrs025@outlook.com>
2017-03-30 21:43:48 +0200

    Final edit

--- spigot/spigot.sh
+++ spigot/spigot.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Spigot config get
-echo "Let's start getting all this things baby !"
+echo -e "
+  __  __       _ _   _                 __ _                 _   _    ____     ____             __ _       
+ |  \/  |_   _| | |_(_) ___ _ __ __ _ / _| |_              | | / \  |  _ \   / ___|___  _ __  / _(_) __ _ 
+ | |\/| | | | | | __| |/ __|  __/ _  | |_| __|  _____   _  | |/ _ \ | |_) | | |   / _ \|  _ \| |_| |/ _  |
+ | |  | | |_| | | |_| | (__| | | (_| |  _| |_  |_____| | |_| / ___ \|  _ <  | |__| (_) | | | |  _| | (_| |
+ |_|  |_|\__,_|_|\__|_|\___|_|  \__,_|_|  \__|          \___/_/   \_\_| \_\  \____\___/|_| |_|_| |_|\__, |
+                                                                                                    |___/ "
+echo -e "\033[0;36m ## \033[0;32mLet's start getting all this things baby ! \033[37m"
 wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-x-latest.jar.conf
 wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.11.2.jar.conf
 wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.10.2.jar.conf
@@ -11,6 +18,6 @@ wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spig
 wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.6.4.jar.conf
 wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.5.2.jar.conf
 wget http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/spigot/spigot-1.4.7.jar.conf
-echo "Well well well, that's the end you get all files ! Great !"
-echo "Goodbye"
+echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33mXXXXXXX \033[0;32mfiles ! Great ! \033[37m"
+echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
 exit
