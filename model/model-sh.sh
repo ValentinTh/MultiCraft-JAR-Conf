45eaf632363533514e0d8888576ed37208226d3b
Valentin T <ins.mrs025@outlook.com>
2017-09-09 21:05:40 +0200

    Don't duplicate new DL

--- model/model-sh.sh
+++ model/model-sh.sh
@@ -8,8 +8,8 @@ echo -e "
  |_|  |_|\__,_|_|\__|_|\___|_|  \__,_|_|  \__|          \___/_/   \_\_| \_\  \____\___/|_| |_|_| |_|\__, |
                                                                                                     |___/ "
 echo -e "\033[0;36m ## \033[0;32mLet's start getting all this things baby ! \033[37m"
-wget -P ./@XXXX http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/xxxx.jar.conf
-wget -P ./@XXXX http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/xxxx.jar.conf
+wget -N -P ./@XXXX http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/xxxx.jar.conf
+wget -N -P ./@XXXX http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/xxxx.jar.conf
 echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33m@XXXXXXX \033[0;32mfiles ! Great ! \033[37m"
 echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
 rm -r @xxxx.sh
