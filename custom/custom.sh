af4ad5ef00c12f0f4216cbe252cf4aab120c97e3
Valentin T <ins.mrs025@outlook.com>
2017-12-29 00:04:47 +0100

    Chown user

--- custom/custom.sh
+++ custom/custom.sh
@@ -9,10 +9,10 @@ echo -e "
                                                                                                     |___/ "
 echo -e "\033[0;36m ## \033[0;32mLet's start getting all this things baby ! \033[37m"
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/custom/my-jar.jar.conf
-wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/custom/my-bungee.jar.conf
-echo -e "\033[0;36m ## \033[0;35mPermissions accorded my general ! \033[0m"
-chown minecraft:minecraft ./jar/my-bungee.jar.conf
 chown minecraft:minecraft ./jar/my-jar.jar.conf
+wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/custom/my-bungee.jar.conf
+chown minecraft:minecraft ./jar/my-bungee.jar.conf
+echo -e "\033[0;36m ## \033[0;35mPermissions accorded my general ! \033[0m"
 echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33mCUSTOM \033[0;32mfiles ! Great ! \033[37m"
 echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
 rm -r custom.sh
