8e817f18a1497f30737ab81ca124b413b793a8c1
Valentin T <ins.mrs025@outlook.com>
2017-12-28 23:06:54 +0100

    Chown user

--- vanilla/vanilla.sh
+++ vanilla/vanilla.sh
@@ -9,16 +9,28 @@ echo -e "
                                                                                                     |___/ "
 echo -e "\033[0;36m ## \033[0;32mLet's start getting all this things baby ! \033[37m"
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.12.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.12.2.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.12.1.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.12.1.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.11.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.11.2.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.10.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.10.2.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.9.4.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.9.4.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.8.9.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.8.9.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.7.10.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.7.10.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.7.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.7.2.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.6.4.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.6.4.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.5.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.5.2.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/vanilla/vanilla-1.4.7.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.4.7.jar.conf
+echo -e "\033[0;36m ## \033[0;35mPermissions accorded my general ! \033[0m"
 echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33mVANILLA \033[0;32mfiles ! Great ! \033[37m"
 echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
 rm -r vanilla.sh
