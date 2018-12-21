c5787b15773f621fb45d82e18b9498538c276071
Valentin T <12909802+ValentinTh@users.noreply.github.com>
2018-12-21 17:50:51 +0100

    1.13.2

--- scripts/spigot.sh
+++ scripts/spigot.sh
@@ -10,6 +10,8 @@ echo -e "
 echo -e "\033[0;36m ## \033[0;32mLet's start getting all these things baby ! \033[37m"
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-x-latest.jar.conf
 chown minecraft:minecraft ./jar/spigot-x-latest.jar.conf
+wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.13.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.13.2.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.12.2.jar.conf
 chown minecraft:minecraft ./jar/spigot-1.12.2.jar.conf
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.12.1.jar.conf
