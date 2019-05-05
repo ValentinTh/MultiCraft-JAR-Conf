64587e89bee7dc2f8ea7f389cb891289cbf1790f
Valentin T <12909802+ValentinTh@users.noreply.github.com>
2019-05-05 18:59:34 +0200

    Create name.php

---
+++ translate/de/name.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'short'=>'DE',    //Short language code, for example 'de'
+    'english'=>'German',  //English name of the language
+    'local'=>'Deutsch',    //Local name of the language
+);
