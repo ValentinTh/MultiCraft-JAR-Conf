0d47fcc52a1d45f40a3cc1592fe84a3b3348907e
Valentin T <12909802+ValentinTh@users.noreply.github.com>
2021-03-16 11:43:50 +0100

    chmod 755

--- scripts/vanilla.sh
+++ scripts/vanilla.sh
@@ -10,60 +10,116 @@ echo -e "
 echo -e "\033[0;36m ## \033[0;32mLet's start getting all these things baby ! \033[37m"
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.5.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.16.5.jar.conf
+chmod 755 ./jar/vanilla-1.16.5.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.4.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.16.4.jar.conf
+chmod 755 ./jar/vanilla-1.16.4.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.3.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.16.3.jar.conf
+chmod 755 ./jar/vanilla-1.16.3.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.16.2.jar.conf
+chmod 755 ./jar/vanilla-1.16.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.1.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.16.1.jar.conf
+chmod 755 ./jar/vanilla-1.16.1.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.16.jar.conf
+chmod 755 ./jar/vanilla-1.16.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.15.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.15.2.jar.conf
+chmod 755 ./jar/vanilla-1.15.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.15.1.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.15.1.jar.conf
+chmod 755 ./jar/vanilla-1.15.1.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.15.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.15.jar.conf
+chmod 755 ./jar/vanilla-1.15.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.4.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.14.4.jar.conf
+chmod 755 ./jar/vanilla-1.14.4.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.3.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.14.3.jar.conf
+chmod 755 ./jar/vanilla-1.14.3.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.14.2.jar.conf
+chmod 755 ./jar/vanilla-1.14.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.1.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.14.1.jar.conf
+chmod 755 ./jar/vanilla-1.14.1.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.14.jar.conf
+chmod 755 ./jar/vanilla-1.14.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.13.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.13.2.jar.conf
+chmod 755 ./jar/vanilla-1.13.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.13.1.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.13.1.jar.conf
+chmod 755 ./jar/vanilla-1.13.1.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.13.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.13.jar.conf
+chmod 755 ./jar/vanilla-1.13.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.12.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.12.2.jar.conf
+chmod 755 ./jar/vanilla-1.12.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.12.1.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.12.1.jar.conf
+chmod 755 ./jar/vanilla-1.12.1.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.11.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.11.2.jar.conf
+chmod 755 ./jar/vanilla-1.11.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.10.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.10.2.jar.conf
+chmod 755 ./jar/vanilla-1.10.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.9.4.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.9.4.jar.conf
+chmod 755 ./jar/vanilla-1.9.4.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.8.9.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.8.9.jar.conf
+chmod 755 ./jar/vanilla-1.8.9.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.7.10.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.7.10.jar.conf
+chmod 755 ./jar/vanilla-1.7.10.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.7.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.7.2.jar.conf
+chmod 755 ./jar/vanilla-1.7.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.6.4.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.6.4.jar.conf
+chmod 755 ./jar/vanilla-1.6.4.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.5.2.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.5.2.jar.conf
+chmod 755 ./jar/vanilla-1.5.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.4.7.jar.conf
 chown minecraft:minecraft ./jar/vanilla-1.4.7.jar.conf
+chmod 755 ./jar/vanilla-1.4.7.jar.conf
+
 echo -e "\033[0;36m ## \033[0;35mPermissions accorded my general ! \033[0m"
 echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33mVANILLA \033[0;32mfiles ! Great ! \033[37m"
 echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
