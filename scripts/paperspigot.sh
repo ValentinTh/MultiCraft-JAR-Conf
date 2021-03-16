28443f5d00cd8b105a3e6daf8533bd4f9ba21ace
Valentin T <12909802+ValentinTh@users.noreply.github.com>
2021-03-16 11:28:37 +0100

    chmod

--- scripts/paperspigot.sh
+++ scripts/paperspigot.sh
@@ -10,22 +10,39 @@ echo -e "
 echo -e "\033[0;36m ## \033[0;32mLet's start getting all these things baby ! \033[37m"
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.16.5.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.16.5.jar.conf
+chmod 755 ./jar/paperspigot-1.16.5.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.16.4.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.16.4.jar.conf
+chmod 755 ./jar/paperspigot-1.16.4.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.16.3.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.16.3.jar.conf
+chmod 755 ./jar/paperspigot-1.16.3.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.15.2.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.15.2.jar.conf
+chmod 755 ./jar/paperspigot-1.15.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.15.1.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.15.1.jar.conf
+chmod 755 ./jar/paperspigot-1.15.1.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.14.4.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.14.4.jar.conf
+chmod 755 ./jar/paperspigot-1.14.4.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.13.2.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.13.2.jar.conf
+chmod 755 ./jar/paperspigot-1.13.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.12.2.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.12.2.jar.conf
+chmod 755 ./jar/paperspigot-1.12.2.jar.conf
+
 wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.8.8.jar.conf
 chown minecraft:minecraft ./jar/paperspigot-1.8.8.jar.conf
+chmod 755 ./jar/paperspigot-1.8.8.jar.conf
 echo -e "\033[0;36m ## \033[0;35mPermissions accorded my general ! \033[0m"
 echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33mPAPER SPIGOT \033[0;32mfiles ! Great ! \033[37m"
 echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
